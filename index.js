f3b3616c71186021a1d19100565413965a9894be
Thiago Medeiros <medeiroshs@hotmail.com>
2022-06-20 18:23:07 -0300

    feat: Requisito 04, adicionando validações para o endpoint /login

--- index.js
+++ index.js
@@ -38,6 +38,9 @@ app.post('/login', (req, res) => {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   const valor = crypto.randomBytes(8).toString('hex');
   const token = {
     token: valor.slice(0, 16),
